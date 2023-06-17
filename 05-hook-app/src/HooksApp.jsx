79320559220ce2d48a395092ff3d72ca576e6358
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-17 19:09:59 -0300

    useFetch

--- 05-hook-app/src/HooksApp.jsx
+++ 05-hook-app/src/HooksApp.jsx
@@ -2,6 +2,7 @@ import { CounterApp } from "./01-useState/CounterApp"
 import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook"
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 
 export const HooksApp = () => {
     return (
@@ -10,8 +11,9 @@ export const HooksApp = () => {
 
             {/* <CounterApp /> */}
             {/* <CounterWithCustomHook /> */}
-            <SimpleForm />
-            <FormWithCustomHook/>
+            {/* <SimpleForm /> */}
+            {/* <FormWithCustomHook /> */}
+            <MultipleCustomHooks />
         </>
     )
 }
