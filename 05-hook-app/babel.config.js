e983132e2a6ccd29e49b05aadc848f90f38de460
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-06-20 00:17:05 -0300

    configure test enviroment

---
+++ 05-hook-app/babel.config.js
@@ -0,0 +1,8 @@
+const babelConfig = {
+    presets: [
+        [ '@babel/preset-env', { targets: { esmodules: true } } ],
+        [ '@babel/preset-react', { runtime: 'automatic' } ],
+    ],
+};
+
+export default babelConfig;
---
+++ 05-hook-app/jest.config.js
@@ -0,0 +1,6 @@
+const jestConfig = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFiles: ['./jest.setup.js']
+}
+
+export default jestConfig;
