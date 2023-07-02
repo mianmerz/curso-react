bd54eac76f459c5282918a8a0bc873a9dafbafb1
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 19:32:15 -0300

    extract characters component

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 
+const CharactersByHero = ({ alter_ego, characters }) => {
+    return (
+        <>
+            {
+                alter_ego !== characters && <p>{characters}</p>
+            }
+        </>
+    )
+}
+
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
     const heroImage = `/assets/heroes/${id}.jpg`;
+
 
     return (
         <div className="col">
@@ -17,9 +28,7 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
                             <h5 className="card-title">{superhero}</h5>
                             <p className="card-text">{alter_ego}</p>
 
-                            {
-                                alter_ego !== characters && <p>{characters}</p>
-                            }
+                            <CharactersByHero alter_ego={alter_ego} characters={characters} /> 
 
                             <p className="card-text">
                                 <small className="text-muted">{first_appearance}</small>
