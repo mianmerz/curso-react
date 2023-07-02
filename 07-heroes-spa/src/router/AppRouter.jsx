6cd3a856633767e7d2d9b6da8fab46175dd1fcb1
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 17:57:43 -0300

    feat: create new pages

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,21 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
-import { LoginPage } from '../auth'
-import { DCPage, MarvelPage } from '../heroes'
-
-import { Navbar } from "../ui"
+import { LoginPage } from '../auth';
+import { HeroesRoutes } from "../heroes";
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
                 <Route path="login" element={<LoginPage />} />
-
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DCPage />} />
-
-                <Route path="/" element={<Navigate to="/marvel" />} />
+                
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
         </>
     )
