3f4554bcd2100c8072d48cd3675fef96aca89e2e
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 03:27:17 -0300

    test: add test TodoItem component - mostrar el Todo Pendiente de completar

--- 05-hook-app/src/08-useReducer/components/TodoItem.jsx
+++ 05-hook-app/src/08-useReducer/components/TodoItem.jsx
@@ -2,8 +2,11 @@
 export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
-            <span className={"user-select-none " + (todo.done ? 'text-decoration-line-through' : '')}
-                onDoubleClick={() => onToggleTodo(todo.id)}> {todo.description}  {todo.done && '(completa)'} </span>
+            <span
+                className={"user-select-none " + (todo.done ? 'text-decoration-line-through' : '')}
+                onDoubleClick={() => onToggleTodo(todo.id)}
+                aria-label="Cambiar estado de la tarea"
+            > {todo.description}  {todo.done && '(completa)'} </span>
 
             <button className="btn btn-danger" onClick={() => onDeleteTodo(todo.id)}>Borrar</button>
         </li>
---
+++ 05-hook-app/tests/08-useReducer/TodoItem.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { TodoItem } from "../../src/08-useReducer/components/TodoItem";
+
+describe('Pruebas en <TodoItem />', () => {
+    const todo = {
+        id: 1,
+        description: 'Aprender React',
+    }
+
+    const onDeleteTodoMock = jest.fn();
+    const onToggleTodoMock = jest.fn();
+
+    beforeEach(() => jest.clearAllMocks());
+
+    test('debe de mostrar el Todo Pendiente de completar', () => {
+        render(<TodoItem todo={todo} onToggleTodo={onDeleteTodoMock} onDeleteTodo={onDeleteTodoMock} />);
+
+        const liElement = screen.getByRole('listitem');
+        expect(liElement.className).toBe('list-group-item d-flex justify-content-between align-items-center')
+
+        const spanElement = screen.getByLabelText('Cambiar estado de la tarea');
+        expect(spanElement.className).toContain('user-select-none')
+        expect(spanElement.className).not.toContain('text-decoration-line-through')
+    });
+
+})
