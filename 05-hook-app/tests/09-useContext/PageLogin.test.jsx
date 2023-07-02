3e1bb41d8003dc43eae8bb8ad49c5c3005c147d7
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 04:24:42 -0300

    test: add test PageHome component - llamar el setUser cuando se hace click en el boton

--- 05-hook-app/tests/09-useContext/PageLogin.test.jsx
+++ 05-hook-app/tests/09-useContext/PageLogin.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { UserContext } from "../../src/09-useContext/context/UserContext";
 import { LoginPage } from "../../src/09-useContext/pages/PageLogin";
@@ -14,5 +14,21 @@ describe('Pruebas en <LoginPage />', () => {
 
         const preTag = screen.getByLabelText('pre');
         expect(preTag.innerHTML).toBe('null');
+    });
+
+    test('debe de llamar el setUser cuando se hace click en el boton', () => {
+        const setUserMock = jest.fn();
+
+        render(
+            <UserContext.Provider value={{ user: null, setUser: setUserMock }}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(setUserMock).toHaveBeenCalled()
+        expect(setUserMock).toHaveBeenCalledWith({ "email": "miguel@gmail.com", "id": 123, "name": "Miguelo" })
     });
 });
