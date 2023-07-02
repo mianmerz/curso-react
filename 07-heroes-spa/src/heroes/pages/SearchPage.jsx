e50188c2bc90d3a2836bf253430b0c02a7338616
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 20:36:29 -0300

    feat: show messages

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -12,6 +12,9 @@ export const SearchPage = () => {
     const { q = "" } = queryString.parse(location.search);
     const heroes = getHeroesByName(q);
 
+    const showSearch = (q === '');
+    const showError = (q !== '' && heroes.length === 0);
+
     const { searchText, onInputChange } = useForm({
         searchText: q
     })
@@ -19,7 +22,7 @@ export const SearchPage = () => {
     const onSearchSubmit = (e) => {
         e.preventDefault();
 
-        if (searchText.trim().length < 1) return;
+        // if (searchText.trim().length < 1) return;
         navigate(`?q=${searchText}`, { replace: true });
     }
 
@@ -50,13 +53,15 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr />
 
-                    <div className="alert alert-primary">
-                        Search a hero
-                    </div>
+                    {/* {
+                        q === ''
+                            ? <div className="alert alert-primary">Search a hero</div>
+                            : heroes.length === 0 && <div className="alert alert-danger">No hero with <b>{q}</b></div>
+                    } */}
 
-                    <div className="alert alert-danger">
-                        No hero with <b>{q}</b>
-                    </div>
+
+                    <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>Search a hero</div>
+                    <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>No hero with <b>{q}</b></div>
 
                     {
                         heroes.map(hero => <HeroCard key={hero.id} {...hero}></HeroCard>)
