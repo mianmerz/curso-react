a1536333b6aca96591181da5b31372ea5df4d508
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-02 19:23:55 -0300

    move assets and show card heroes

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,3 +1,4 @@
+import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 
 export const HeroList = ({ publisher }) => {
@@ -5,12 +6,14 @@ export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>{hero.superhero}</li>
+                    <HeroCard key={hero.id} {...hero}></HeroCard>
                 ))
             }
-        </ul>
+        </div>
     )
 }
+
+
