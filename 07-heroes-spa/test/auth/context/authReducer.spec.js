fa89d049e2f972929ba90a43d9abc4f5b64de492
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-23 20:53:56 -0300

    test: add debe de autenticar y colocar el name del usuario

--- 07-heroes-spa/test/auth/context/authReducer.spec.js
+++ 07-heroes-spa/test/auth/context/authReducer.spec.js
@@ -1,5 +1,5 @@
 
-import { authReducer } from '../../../src/auth/context/authReducer';
+import { authReducer, types } from '../../../src/auth';
 
 describe('Pruebas en authReducer', () => {
 
@@ -10,7 +10,13 @@ describe('Pruebas en authReducer', () => {
     })
 
     test('debe de autenticar y colocar el name del usuario', () => {
+        const action = {
+            type: types.login,
+            payload: { name: "Juan", id: "123" }
+        }
 
+        const state = authReducer({ logged: false }, action);
+        expect(state).toEqual({ logged: true, user: action.payload})
     })
 
     test('debe de borrar el name del usuario y logged en false', () => {
