95d602e308760138c776b4a025fd31130c2d5d47
Miguel Merelli <miguel.merelli@mercadolibre.com>
2023-07-23 22:16:01 -0300

    feat: JournalApp - Initial scafolding

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,41 +1,54 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom"
-import { getHeroById } from "../helpers";
-import { useMemo } from "react";
+import { useMemo } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+
 
 export const HeroPage = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
 
-    const hero = useMemo(() => getHeroById(id), [id]);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const onNavigateBack = () => navigate(-1);
+  const hero = useMemo( () => getHeroById( id ), [ id ]); 
 
-    if (!hero) return <Navigate to="/" />
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
 
-    return (
-        <>
-            <div className="row mt-5">
-                <div className="col-4">
-                    <img className="img-thumbnail animate__animated animate__fadeInLeft" src={`/assets/heroes/${id}.jpg`} alt={hero.superhero} />
-                </div>
 
-                <div className="col">
-                    <h3>{hero.superhero}</h3>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
-                        <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
-                        <li className="list-group-item"><b>First appearance:</b> {hero.first_appearance}</li>
-                    </ul>
+  if ( !hero ) {
+    return <Navigate to="/marvel" />
+  }
+  
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={ `/assets/heroes/${ id }.jpg` } 
+          alt={ hero.superhero }
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
 
-                    <h5 className="mt-3">Characters</h5>
-                    <p>{hero.characters}</p>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego:</b> { hero.alter_ego } </li>
+          <li className="list-group-item"> <b>Publisher:</b> { hero.publisher } </li>
+          <li className="list-group-item"> <b>First appearance:</b> { hero.first_appearance } </li>
+        </ul>
 
-                    <button className="btn btn-outline-primary"
-                        onClick={onNavigateBack}>
-                        Regresar
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+        <h5 className="mt-3"> Characters </h5>
+        <p>{ hero.characters }</p>
+
+        <button 
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+        >
+          Regresar
+        </button>
+
+      </div>
+
+    </div>
+  )
 }
--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,73 +1,94 @@
-import queryString from 'query-string';
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string'
 
-import { HeroCard } from "../components"
-import { useForm } from "../../hooks/useForm";
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../components';
 import { getHeroesByName } from '../helpers';
 
 export const SearchPage = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
 
-    const { q = "" } = queryString.parse(location.search);
-    const heroes = getHeroesByName(q);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const showSearch = (q === '');
-    const showError = (q !== '' && heroes.length === 0);
+  const { q = '' } = queryString.parse( location.search );
+  const heroes = getHeroesByName(q);
 
-    const { searchText, onInputChange } = useForm({
-        searchText: q
-    })
+  const showSearch = (q.length === 0);
+  const showError  = (q.length > 0) && heroes.length === 0;
 
-    const onSearchSubmit = (e) => {
-        e.preventDefault();
 
-        // if (searchText.trim().length < 1) return;
-        navigate(`?q=${searchText}`, { replace: true });
-    }
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
 
-    return (
-        <>
-            <h1>Search</h1>
+
+
+  const onSearchSubmit = (event) =>{
+    event.preventDefault();
+    // if ( searchText.trim().length <= 1 ) return;
+    navigate(`?q=${ searchText }`);
+  }
+
+
+  return (
+    <>
+      <h1>Search</h1> 
+      <hr />
+
+      <div className="row">
+
+          <div className="col-5">
+            <h4>Searching</h4>
+            <hr />
+            <form onSubmit={ onSearchSubmit } aria-label="form">
+              <input 
+                type="text"
+                placeholder="Search a hero"
+                className="form-control"
+                name="searchText"
+                autoComplete="off"
+                value={ searchText }
+                onChange={ onInputChange }
+              />
+
+              <button className="btn btn-outline-primary mt-1">
+                Search
+              </button>
+            </form>
+          </div>
+
+          <div className="col-7">
+            <h4>Results</h4>
             <hr />
 
-            <div className="row">
-                <div className="col-5">
-                    <h4>Searching</h4>
-                    <hr />
-
-                    <form onSubmit={onSearchSubmit}>
-                        <input type="text"
-                            placeholder="Find your hero"
-                            className="form-control"
-                            name="searchText"
-                            autoComplete="off"
-                            value={searchText}
-                            onChange={onInputChange}
-                        />
-                        <button className="btn btn-outline-primary mt-3">Search</button>
-                    </form>
-                </div>
-
-                <div className="col-7">
-                    <h4>Results</h4>
-                    <hr />
-
-                    {/* {
-                        q === ''
-                            ? <div className="alert alert-primary">Search a hero</div>
-                            : heroes.length === 0 && <div className="alert alert-danger">No hero with <b>{q}</b></div>
-                    } */}
-
-
-                    <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>Search a hero</div>
-                    <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>No hero with <b>{q}</b></div>
-
-                    {
-                        heroes.map(hero => <HeroCard key={hero.id} {...hero}></HeroCard>)
-                    }
-                </div>
+            {/* {
+              ( q === '' )
+                ? <div className="alert alert-primary">Search a hero</div>
+                : ( heroes.length === 0 ) 
+                  && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+            } */}
+            
+            <div className="alert alert-primary animate__animated animate__fadeIn" 
+                style={{ display: showSearch ? '' : 'none' }}>
+              Search a hero
             </div>
-        </>
-    )
+
+            <div aria-label="alert-danger" className="alert alert-danger animate__animated animate__fadeIn" 
+                style={{ display: showError ? '' : 'none' }}>
+              No hero with <b>{ q }</b>
+            </div>
+
+
+            {
+              heroes.map( hero => (
+                <HeroCard key={ hero.id } {...hero } />
+              ))
+            }
+
+          </div>
+      </div>
+      
+
+    </>
+  )
 }
